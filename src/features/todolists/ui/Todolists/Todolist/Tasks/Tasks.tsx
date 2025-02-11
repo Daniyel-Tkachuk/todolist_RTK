19227812a1b0bd6969208df4c2310ae482cd0511
Daniyel-Tkachuk <danilaradyn7357899@gmail.com>
2025-02-11 15:53:34 +0300

    optimization

--- src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -2,35 +2,44 @@ import List from "@mui/material/List"
 import { useAppSelector } from "common/hooks/useAppSelector"
 import { selectTasks } from "../../../../model/tasksSelectors"
 import { Task } from "./Task/Task"
-import { TodolistType } from "../../../../model/todolists-reducer"
+import { DomainTodolist } from "../../../../model/todolists-reducer"
+import { useEffect } from "react"
+import { useAppDispatch } from "common/hooks"
+import { fetchTasksThunkTC } from "../../../../model/tasks-reducer"
+import { TaskStatus } from "common/enums"
 
 type Props = {
-  todolist: TodolistType
+  todolist: DomainTodolist
 }
 
 export const Tasks = ({ todolist }: Props) => {
   const tasks = useAppSelector(selectTasks)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchTasksThunkTC(todolist.id))
+  }, [])
 
   const allTodolistTasks = tasks[todolist.id]
 
   let tasksForTodolist = allTodolistTasks
 
   if (todolist.filter === "active") {
-    tasksForTodolist = allTodolistTasks.filter((task) => !task.isDone)
+    tasksForTodolist = allTodolistTasks.filter((task) => task.status === TaskStatus.New)
   }
 
   if (todolist.filter === "completed") {
-    tasksForTodolist = allTodolistTasks.filter((task) => task.isDone)
+    tasksForTodolist = allTodolistTasks.filter((task) => task.status === TaskStatus.Completed)
   }
 
   return (
     <>
-      {tasksForTodolist.length === 0 ? (
+      {tasksForTodolist && tasksForTodolist.length === 0 ? (
         <p>Тасок нет</p>
       ) : (
         <List>
-          {tasksForTodolist.map((task) => {
-            return <Task task={task} todolist={todolist} />
+          {tasksForTodolist?.map((task) => {
+            return <Task key={task.id} task={task} todolist={todolist} />
           })}
         </List>
       )}
